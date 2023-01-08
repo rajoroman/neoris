15a1e8fe7ad80aedfab0f967c5a7c14b63777c20
rajoroman <51220078+rajoroman@users.noreply.github.com>
2023-01-08 14:02:59 -0500

    Add files via upload

---
+++ docker-compose/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.gender(id, description)VALUES(1, 'MASCULINO');
+INSERT INTO public.gender(id, description)VALUES(2, 'FEMENINO');
+INSERT INTO public.gender(id, description)VALUES(3, 'OTROS');
+
+INSERT INTO public.account_type(id, description)VALUES(1, 'AHORROS');
+INSERT INTO public.account_type(id, description)VALUES(2, 'CORRIENTE');
+
